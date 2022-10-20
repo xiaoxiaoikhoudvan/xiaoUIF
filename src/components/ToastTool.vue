4ba917eabcbf5500f074bb649b4403d7b28d9800
xiaoxiaoikhoudvan <752998553@qq.com>
2022-10-20 21:58:49 +0800

    Toast

--- src/components/ToastTool.vue
+++ src/components/ToastTool.vue
@@ -1,15 +1,29 @@
 <template>
-    <button @click="click1">dddd</button>
+    <x-button @click="click1">默认</x-button>
+    <x-button @click="click2">成功</x-button>
+    <x-button @click="click3">警告</x-button>
+    <x-button @click="click4">错误</x-button>
 </template>
 <script lang="ts">
 import {onMounted,inject} from 'vue'
+import Button from '../lib/Button.vue'
 import {showMsg} from '../toastPlugin'
 export default {
+    components:{'x-button':Button},
     setup(props,context) {
         const click1 = ()=>{
-            showMsg({text:'jjjjj',type:'uuuuu'})
+            showMsg({text:'这是一条普通消息',type:'normal'})
         }
-        return {click1}
+        const click2 = ()=>{
+            showMsg({text:'这是一条成功消息',type:'success'})
+        }
+        const click3 = ()=>{
+            showMsg({text:'这是一条警告消息',type:'warning'})
+        }
+        const click4 = ()=>{
+            showMsg({text:'这是一条错误消息',type:'error'})
+        }
+        return {click1,click2,click3,click4}
     }
 }
 </script>
--- src/lib/Toast.vue
+++ src/lib/Toast.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="toast">
+    <div class="xiao-coast" :class="classes">
         {{text}}
     </div>
 </template>
 <script lang="ts">
+import {computed} from 'vue'
 export default {
     name:'xiaoToast',
     props:{
@@ -17,22 +18,60 @@ export default {
     },
     setup(props) {
         console.log("props=>",props);
-        
+        const classes = computed(()=>{
+            return {
+                [`xiao-coast-${props.type}`]:props.type,
+            }
+        })
+        return{classes}
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    .toast{
-        // border: 1px solid red;
-        // height:'40px';
-        // border:'1px solid black';
-        // fontSize:'14px';
-        // position:'fixed';
-        // top:200px;
-        // left:'50%',
-        // padding:'8px 24px',
-        // display:'flex',
-        // alignItems:'center',
+    $font-size:14px;
+    $toast-height:40px;
+    $border-radius:4px;
+    $normal-bg:#EDF2FC;
+    $normal-textColor:#909399;
+    $normal-border:#EBEEF5;
+    $success-bg:#F0F9EB;
+    $success-textColor:#67C23A;
+    $success-border:#E1F3D8;
+    $warning-bg:#FDF6EC;
+    $warning-textColor:#E6A23C;
+    $warning-border:#FAECD8;
+    $error-bg:#FEF0F0;
+    $error-textColor:#F56C6C;
+    $error-border:#FDE2E2;
+    .xiao-coast{
+        height:$toast-height;
+        font-size:$font-size;
+        position:fixed;
+        top:78px;
+        left:50%;
+        padding:8px 24px;
+        border-radius: 4px;
+        display:flex;
+        align-items:center;
+        &.xiao-coast-normal{
+            background: $normal-bg;
+            color: $normal-textColor;
+        }
+        &.xiao-coast-success{
+            background: $success-bg;
+            color: $success-textColor;
+            border: 1px solid $success-border;
+        }
+        &.xiao-coast-warning{
+            background: $warning-bg;
+            color: $warning-textColor;
+            border: 1px solid $warning-border;
+        }
+        &.xiao-coast-error{
+            background: $error-bg;
+            color: $error-textColor;
+            border: 1px solid $error-border;
+        }
     }
 </style>
