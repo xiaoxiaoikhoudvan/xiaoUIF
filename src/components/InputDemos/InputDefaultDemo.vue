c44dee17eb7d63035dc74b91dcd15dcd452f44df
xiaoxiaoikhoudvan <752998553@qq.com>
2022-10-25 22:23:52 +0800

    input

---
+++ src/components/InputDemos/InputDefaultDemo.vue
@@ -0,0 +1,37 @@
+<demo>
+基础用法
+</demo>
+<template>
+    <Input v-model:value="value" @change="inputChange" />
+    <p>{{value}}</p>
+</template>
+
+<script lang="ts">
+import Input from '../../lib/Input.vue'
+import {ref} from 'vue'
+export default {
+    components:{
+        'x-input':Input
+    },
+    setup(props) {
+        const value = ref(null)
+        return{value}
+    }
+}
+</script>
+<style lang="scss" scoped>
+    .xiao-grid-content{
+        border-radius: 4px;
+        min-height: 36px;
+        margin-bottom: 20px;
+    }
+    .bg-purple-dark{
+        background: #99a9bf;
+    }
+    .bg-purple {
+        background: #d3dce6;
+    }
+    .bg-purple-light {
+        background: #e5e9f2;
+    }
+</style>
--- src/components/InputTool.vue
+++ src/components/InputTool.vue
@@ -1,16 +1,21 @@
 <template>
     <div>
-        <h1>Input 输入框</h1>
-        <Input value="战三" @change="inputChange" />
+        <Demo :component="InputDefaultDemo" />
+        <!-- <h1>Input 输入框</h1> -->
+        <!-- <Input value="战三" @change="inputChange" />
         <Input disabled />
         <Input value="王五" readonly />
-        <Input value="李四" error="姓名难听" />
+        <Input value="李四" error="姓名难听" /> -->
     </div>
 </template>
 <script lang="ts">
 import Demo from './Demo.vue'
+import InputDefaultDemo from './InputDemos/InputDefaultDemo.vue'
 import Input from '../lib/Input.vue'
 export default {
-    components:{Input}
+    components:{Demo},
+    setup(props) {
+        return{InputDefaultDemo}
+    }
 }
 </script>
